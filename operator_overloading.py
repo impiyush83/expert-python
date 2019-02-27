b6a783fd4555eb337d900e47c6a04c1098b52a92
nalawadepiyush <nalawadepiyush@gmail.com>
2019-02-27 15:09:54 +0530

    Operator overloading and magic fucntions explained

---
+++ operator_overloading.py
@@ -0,0 +1,50 @@
+# operators cannot be used to do manipulations on objects
+
+
+# operqtor overloading can be done in python with help of magic functions
+
+
+class complex:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+        # adding two objects
+
+    def __add__(self, other):
+        return self.a + other.a, self.b + other.b
+
+
+Ob1 = complex(1, 2)
+Ob2 = complex(2, 3)
+Ob3 = Ob1 + Ob2
+print(Ob3)
+
+
+"""
+
+Python magic functions for operator overloading
+OPERATOR	MAGIC METHOD
+
++	__add__(self, other)
+–	__sub__(self, other)
+*	__mul__(self, other)
+/	__truediv__(self, other)
+//	__floordiv__(self, other)
+%	__mod__(self, other)
+**	__pow__(self, other)
+<	__lt__(self, other)
+>	__gt__(self, other)
+<=	__le__(self, other)
+>=	__ge__(self, other)
+==	__eq__(self, other)
+!=	__ne__(self, other)
+-=	__isub__(self, other)
++=	__iadd__(self, other)
+*=	__imul__(self, other)
+/=	__idiv__(self, other)
+//=	__ifloordiv__(self, other)
+%=	__imod__(self, other)
+**=	__ipow__(self, other)
+
+"""
