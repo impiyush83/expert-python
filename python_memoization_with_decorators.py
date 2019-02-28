a2007fc04e737a737a1090e59a1f1b9095f07222
nalawadepiyush <nalawadepiyush@gmail.com>
2019-02-28 14:38:11 +0530

    Memoization with python decorators

---
+++ python_memoization_with_decorators.py
@@ -0,0 +1,24 @@
+def memoize(f):
+    memo = {}
+
+    def helper(x, string):
+        # always 1 is printed as string
+        if x not in memo:
+            memo[x] = f(x, string)
+            # goes through callback stack
+        # finally returns memo {1: 1, 0: 0, 2: 1, 3: 2, 4: 3} for n = 5
+        return memo[x]
+    return helper
+
+
+@memoize
+def fib(n, string):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1, "1") + fib(n - 2, "2")
+
+
+print(fib(5, "try"))
